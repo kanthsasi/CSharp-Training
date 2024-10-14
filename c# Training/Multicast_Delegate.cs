78ada980a8c0e4488131d7d16cb87063ab3fb580
kanthsasi <kanthsasi2003@gmail.com>
2024-10-14 15:51:45 +0530

    14-10-2024

---
+++ c# Training/Multicast_Delegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Training_2
+{
+    internal class Multicast_Delegate
+    {
+        public delegate void SampleDelegate();
+        public static void SampleMethod1()
+        {
+            Console.WriteLine("SampleMethod1 Invoked");
+        }
+        public static void SampleMethod2()
+        {
+            Console.WriteLine("SampleMethod2 Invoked");
+        }
+        public static void Main()
+        {
+            
+            SampleDelegate s = new SampleDelegate(SampleMethod1);
+            s += SampleMethod2;
+            s();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ c# Training/Preventing_Exception_Handling_Abuse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Training_2
+{
+    internal class Preventing_Exception_Handling_Abuse
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter the First Number:");
+                int numerator;
+                bool IsNumeratorConversionIsSuccesfully = Int32.TryParse(Console.ReadLine(), out numerator);
+                if (IsNumeratorConversionIsSuccesfully)
+                {
+                    Console.WriteLine("Enter the Second Number:");
+                    int Denominator;
+                    bool IsDenominatorConversionIsSuccesfully = Int32.TryParse(Console.ReadLine(), out Denominator);
+                    if (IsDenominatorConversionIsSuccesfully && Denominator != 0)
+                    {
+                        int Result = numerator / Denominator;
+                        Console.WriteLine(Result);
+                    }
+                    else
+                    {
+                        if (Denominator == 0)
+                        {
+                            Console.WriteLine("Denominator cannot be zero");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("numerator should be valid number"+Int32.MinValue,Int32.MaxValue);
+                        }
+                    }
+                }
+            }
+           
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+    
+
